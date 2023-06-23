d18231a7bba80e3855e8d9d3a123d70d6ba718bf
Danish Majeed <danishmajeed@spyresync.com>
2023-06-23 12:17:38 +0500

    mongourl

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 const ACCESS_TOKEN_SECERT = "my_secret_expensetracker";
 const PORT = 8080;
 const MONGO_URL =
-  "mongodb+srv://danish_dev:applE11!@cluster0.t8soemq.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://danish_dev:applE11!@cluster0.t8soemq.mongodb.net/?retryWrites=true&w=majority/expenseTracker";
 
 module.exports = { ACCESS_TOKEN_SECERT, PORT, MONGO_URL };
